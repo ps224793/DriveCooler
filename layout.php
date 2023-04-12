2fb17d7160a97a45dd0ef3d71fc3996e18f2c6d3
Bram van Gool <bram.vangool040@gmail.com>
2023-04-12 16:10:02 +0200

    layout update

--- layout.php
+++ layout.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" href=
+"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
-    <div class="col-4 bg-danger">
+<body class="col">
+    <div class="col col-4 bg-danger">
 
     </div>
-    <div class="col-4 bg-dark">
+    <div class="col col-7 bg-dark">
     <span class="material-icons orange600">face</span>
     </div>
 
