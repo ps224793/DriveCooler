7f742360207f7de4d9bb40c5c9914d1725920467
LuukPaans <47414476+LuukPaans@users.noreply.github.com>
2023-04-12 16:44:57 +0200

    added database functions

--- DBcooler.php
+++ DBcooler.php
@@ -1,6 +1,79 @@
 <?php 
+    class DriveCoolerDB
+    {
+        const DSN = "mysql:host=localhost;dbname=drivecooler";
+        const USER = "root";
+        const PASSWD = "";
 
+        //haal alle trainingen van een user op
+        function GetTrainingByUser($userId){		
+            $pdo = new PDO(self::DSN, self::USER, self::PASSWD);
 
+            $statement = $pdo->prepare("SELECT * FROM `usertraing` WHERE `userId` = :userId");  
+            
+            $statement->bindValue(":userId", $userId, PDO::PARAM_INT);
 
+            $statement->execute(); 
+            
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $rows;
+        }
 
+        //haal alle trainingen  op
+        function GetTraining(){		
+            $pdo = new PDO(self::DSN, self::USER, self::PASSWD);
+
+            $statement = $pdo->prepare("SELECT * FROM `training`");  
+
+            $statement->execute(); 
+            
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $rows;
+        }
+
+        //haal 1 user op 
+        function GetUser($userId){		
+            $pdo = new PDO(self::DSN, self::USER, self::PASSWD);
+
+            $statement = $pdo->prepare("SELECT * FROM `user` WHERE `id`= :userId");  
+            
+            $statement->bindValue(":userId", $userId, PDO::PARAM_INT);
+
+            $statement->execute(); 
+            
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $rows;
+        }
+        
+        //haal alle users op
+        function GetUsers(){		
+            $pdo = new PDO(self::DSN, self::USER, self::PASSWD);
+
+            $statement = $pdo->prepare("SELECT * FROM `user`");  
+
+            $statement->execute(); 
+            
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $rows;
+        }
+
+        //haal 1 user op 
+        function GetRatingByUser($userId){		
+            $pdo = new PDO(self::DSN, self::USER, self::PASSWD);
+
+            $statement = $pdo->prepare("SELECT * FROM `ratings` WHERE `userId` = :userId");  
+            
+            $statement->bindValue(":userId", $userId, PDO::PARAM_INT);
+
+            $statement->execute(); 
+            
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $rows;
+        }
+    }
 ?>
