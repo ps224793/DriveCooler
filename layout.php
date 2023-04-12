e199d14d16ca0b1d0024a315511255aaa93bc787
Bram van Gool <bram.vangool040@gmail.com>
2023-04-12 15:58:55 +0200

    layout start

---
+++ layout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="col-4 bg-danger">
+
+    </div>
+    <div class="col-4 bg-dark">
+    <span class="material-icons orange600">face</span>
+    </div>
+
+</body>
+</html>
