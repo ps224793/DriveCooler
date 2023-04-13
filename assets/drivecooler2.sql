bd252c89abaf551ba128e15f5ea122e8cd0b13b4
Rohan Mahadew <ps245000@summacollege.nl>
2023-04-13 13:34:02 +0200

    fixed image

---
+++ assets/drivecooler2.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 13, 2023 at 11:15 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `drivecooler`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Rijgedrag'),
+(2, 'Koeling'),
+(3, 'Brandstof');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+DROP TABLE IF EXISTS `ratings`;
+CREATE TABLE IF NOT EXISTS `ratings` (
+  `userId` int(11) NOT NULL,
+  `catagorieId` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  PRIMARY KEY (`userId`,`catagorieId`),
+  KEY `catagoryId` (`catagorieId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ratings`
+--
+
+INSERT INTO `ratings` (`userId`, `catagorieId`, `rating`) VALUES
+(1, 1, 71),
+(1, 2, 91),
+(1, 3, 69);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `training`
+--
+
+DROP TABLE IF EXISTS `training`;
+CREATE TABLE IF NOT EXISTS `training` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `catagorieId` int(255) NOT NULL,
+  `description` varchar(2550) NOT NULL,
+  `url` varchar(1000) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `imgLocation` varchar(1000) NOT NULL,
+  `videoEmbed` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `catagorieTraining` (`catagorieId`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `training`
+--
+
+INSERT INTO `training` (`id`, `catagorieId`, `description`, `url`, `name`, `imgLocation`, `videoEmbed`) VALUES
+(13, 3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '', 'Versnellingen', 'slipsign.png', 'https://www.youtube.com/embed/qQweWffNuD8'),
+(14, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '', 'Veiligheid', 'versnellingen.png', 'https://www.youtube.com/embed/73Z8omcM2NQ'),
+(15, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '', 'Boetes Vermijden', 'klant.png', 'https://www.youtube.com/watch?v=p2bsA47FtEE'),
+(16, 2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '', 'Koeling', 'koeling1', 'https://www.youtube.com/watch?v=1r26Maz82kY'),
+(17, 2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '', 'Koeling 2', 'koeling2', 'https://www.youtube.com/watch?v=k3t7aCH_C9Q'),
+(18, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '', 'Snelheid', 'slipsign.png', 'https://www.youtube.com/embed/LvhfggKiSHk');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Naam` varchar(255) NOT NULL,
+  `Management` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `Naam`, `Management`) VALUES
+(1, 'Hans', 0),
+(2, 'Bob', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertraing`
+--
+
+DROP TABLE IF EXISTS `usertraing`;
+CREATE TABLE IF NOT EXISTS `usertraing` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `trainingID` int(11) NOT NULL,
+  `completed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `UserTrainingId` (`userId`),
+  KEY `TraingUserId` (`trainingID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `usertraing`
+--
+
+INSERT INTO `usertraing` (`id`, `userId`, `trainingID`, `completed`) VALUES
+(1, 1, 14, 1),
+(2, 1, 15, 0);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD CONSTRAINT `catagoryId` FOREIGN KEY (`catagorieId`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `userId` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `training`
+--
+ALTER TABLE `training`
+  ADD CONSTRAINT `catagorieTraining` FOREIGN KEY (`catagorieId`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `usertraing`
+--
+ALTER TABLE `usertraing`
+  ADD CONSTRAINT `TraingUserId` FOREIGN KEY (`trainingID`) REFERENCES `training` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `UserTrainingId` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
