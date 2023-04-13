222bdd8b0f6a79498e292f9e00fd688b7b69e3f2
Bram van Gool <bram.vangool040@gmail.com>
2023-04-13 13:05:15 +0200

    ik weet het niet meer

--- admin.php
+++ admin.php
@@ -12,30 +12,9 @@
 </head>
 <?php
 include_once "bodylayout.html";
-?>
-<div class="pt-4">
-    <div class="pl-5 row">
-        <img class="border border-secondary" src="assets/bob.png" height="125" width="125">
-        <h3 class="pt-5 pl-3 pb-5">Welkom, Bob</h3>
-    </div>
-    <hr style="height:2px;border-width:0;color:black;background-color:black;width: 1000px;">
-    <h2 class="pl-3 pt-3">Personeels overzicht</h2>
-<!--    CHANGE ME TOO HANS ADMIN PAGE-->
-    <div class="pl-5 pt-2">
-        <h3><a style="color: black; text-decoration: none"  href="home.php">Hans</a></h3>
-        <h3 class="pt-1">Lorem</h3>
-        <h3 class="pt-1">Ipsum</h3>
-        <h3 class="pt-1">Mlesuada</h3>
-        <h3 class="pt-1">Bibendum</h3>
-        <h3 class="pt-1">Arcu</h3>
-        <h3 class="pt-1">Vitae</h3>
-        <h3 class="pt-1">Elementum</h3>
-        <h3 class="pt-1">Curabitur</h3>
-        <h3 class="pt-1">Nunc</h3>
-    </div>
-</div>
 
-<?php
+include_once "adminlayout.html";
+
 include_once "scriptimports.html";
 ?>
 </html>
