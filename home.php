26e25bb0d3b68d3ecc32a1bb5d8e5ae312d5dd0c
Bram van Gool <bram.vangool040@gmail.com>
2023-04-13 13:30:10 +0200

    a

--- home.php
+++ home.php
@@ -4,6 +4,7 @@ $db = new DriveCoolerDB();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,74 +17,40 @@ $db = new DriveCoolerDB();
 <?php
 include "./bodylayout.html";
 ?>
-<div>
-    <?php
-    $rows = $db->GetAllTraining();
+<?php
+$rows = $db->GetAllTraining();
 
-    ?>
-    <div class="pl-5 row">
-        <img src="assets/hans.jpg" width="100" height="100" style="position: fixed; top: 120px" class="border border-secondary">
-        <div style="position: fixed; left: 150px; top: 120px;">
-            <h2 class="pl-3">Welkom Hans!</h2>
-            <span style="font-size: 40px;" class="material-icons pl-2">thermostat</span>
-            <span style="font-size: 40px;" class="material-icons">local_gas_station</span>
-            <span style="font-size: 40px;" class="material-icons">directions_car</span>
-        </div>
-    </div>
-    <br>
-    <div style="height: 120px"></div>
-    <hr style="height:2px;border-width:0;color:black;background-color:black;width: 1000px;">
-    <div class="pl-4">
-        <h2>Trainingen</h2>
-    </div>
+?>
+<div class="pl-5 pt-4 col-md-3 col-12">
+    <img src="assets/hans.jpg" width="100" height="100" class="border border-secondary">
     <div>
-        <?php 
-            $rows = $db->GetAllTraining();
-            foreach($rows as $row){
-                echo '<div class="col-4">';
-                    echo "<p>$row[name]<p>";
-                    echo "<img src=assets/$row[imgLocation] >";
-                    echo "<a href=http://drivecooler/Training.php?TrainingId=$row[id] class=stretched-link> <a/>";
-                echo '</div>';
-            }
-        ?>
-        <!-- <div class="row pl-5">
-            <h5 style="width: 200px">
-            </h5>
-            <h5>Training Two</h5>
-        </div>
-        <div class="row pl-5">
-            <img src="assets/black.png" style="height: 125px; width: 125px">
-            <div style="width: 75px"></div>
-            <img src="assets/black.png" style="height: 125px; width: 125px">
-        </div>
-        <div style="height: 15px"></div>
-        <div class="row pl-5">
-            <h5 style="width: 200px">Training Three</h5>
-            <h5>Training Four</h5>
-        </div>
-        <div class="row pl-5">
-            <img src="assets/black.png" style="height: 125px; width: 125px">
-            <div style="width: 75px"></div>
-            <img src="assets/black.png" style="height: 125px; width: 125px">
-        </div>
-        <div style="height: 15px"></div>
-        <div class="row pl-5">
-            <h5 style="width: 200px">Training Five</h5>
-            <h5>Training Six</h5>
-        </div>
-        <div class="row pl-5">
-            <img src="assets/black.png" style="height: 125px; width: 125px">
-            <div style="width: 75px"></div>
-            <img src="assets/black.png" style="height: 125px; width: 125px">
-        </div> -->
+        <h2 class="pl-3">Welkom Hans!</h2>
+        <span style="font-size: 40px;" class="material-icons pl-2">thermostat</span>
+        <span style="font-size: 40px;" class="material-icons">local_gas_station</span>
+        <span style="font-size: 40px;" class="material-icons">directions_car</span>
     </div>
 </div>
-<?php
-include_once "./bodylayout.html";
-?>
-<?php
-include_once "./scriptimports.html";
-?>
-</body>
+<br>
+<div class="pl-4 col-12 col-md-9">
+    <h2>Trainingen</h2>
+    <div class="row w-100">
+            <?php
+            $rows = $db->GetAllTraining();
+            foreach ($rows as $row) {
+                echo '<div style="max-width:300px;max-height:300px" class="col-12 col-md-4 m-3 overflow-hidden border">';
+                echo "<p>$row[name]<p>";
+                echo "<img src=assets/$row[imgLocation] >";
+                echo "<a href=http://drivecooler/Training.php?TrainingId=$row[id] class=stretched-link> <a/>";
+                echo '</div>';
+            }
+            ?>
+    </div>
+    <?php
+    include_once "./bodylayout.html";
+    ?>
+    <?php
+    include_once "./scriptimports.html";
+    ?>
+    </body>
+
 </html>
