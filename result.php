04bd52117d9f60ec8de3be761beeaad98f3d224c
Rohan Mahadew <ps245000@summacollege.nl>
2023-04-13 10:12:15 +0200

    fixed .

---
+++ result.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <?php
+    include_once "metaimports.html";
+    ?>
+    <title>Document</title>
+</head>
+<?php
+include_once "bodylayout.html";
+?>
+<div>
+    <div class="pl-4">
+        <h2 style="font-size: 25px">Gedragsbeoordelingen:</h2>
+        <div style="height: 60px">
+            <div style="width: 325px; height: 40px" class="row">
+                <span style="font-size: 35px" class="material-icons position-relative mt-1">thermostat</span>
+                <p class="col bg-secondary p-1">You saved 4 trees this month!</p>
+            </div>
+        </div>
+        <div style="height: 60px">
+            <div style="width: 325px; heig" class="row">
+                <span style="font-size: 35px" class="material-icons position-relative">local_gas_station</span>
+                <p class="col bg-secondary p-1">Fuel usage has increased this month!</p>
+            </div>
+        </div>
+        <div style="height: 60px">
+            <div style="width: 325px; height: 40px" class="row">
+                <span style="font-size: 35px" class="material-icons position-relative">directions_car</span>
+                <p class="col bg-secondary p-1">Driving did not improve!</p>
+            </div>
+        </div>
+        <h2 style="font-size: 25px">Resultaten:</h2>
+        <div style="height: 10px"></div>
+        <div style="height: 50px; ">
+            <div style="width: 325px; height: 40px;" class="row">
+                <p class="col bg-secondary p-1">Result</p>
+            </div>
+        </div>
+        <div style="height: 50px;">
+            <div style="width: 325px; height: 40px; padding-left: 35px" class="row">
+                <p class="col bg-secondary p-1">Result</p>
+            </div>
+        </div>
+        <div style="height: 50px;">
+            <div style="width: 325px; height: 40px; padding-left: 35px" class="row">
+                <p class="col bg-secondary p-1">Result</p>
+            </div>
+        </div>
+        <div style="height: 50px;">
+            <div style="width: 325px; height: 40px; padding-left: 35px" class="row">
+                <p class="col bg-secondary p-1">Result</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<?php
+include_once "scriptimports.html";
+?>
+</html>
